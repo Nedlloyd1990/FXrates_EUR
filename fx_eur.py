da2b8a44915982090be94209ad149e551d62d4d4
Nedlloyd Misquitta <47329635+Nedlloyd1990@users.noreply.github.com>
2022-06-05 16:06:41 +0530

    Update fx_eur.py

--- fx_eur.py
+++ fx_eur.py
@@ -21,9 +21,11 @@ options = st.sidebar.multiselect(
      ["AFN","AOA","AMD","SHP","AZN","BYN","BMD","BTN","USD","BAM","BRL","KYD","CDF","ANG","ERN","FKP","FJD","GEL","GHS","GIP","GYD","IRR","KPW","KGS","LRD","MRU","MNT","MZN","RON","WST","STN","RSD","SBD","SSP","SDG","SRD","SYP","TJS","TOP","TMT","VUV","VES","VED","ZMW","ZAR","YER","XPF","XCD","XAF","VND","SGD","UZS","UYU","UGX","UAH","TZS","TTD","TRY","TND","THB","SZL","SOS","SLL","SEK","SCR","SAR","RWF","RUB","QAR","PYG","PLN","PKR","PHP","PGK","PEN","PAB","OMR","NZD","NPR","NOK","NIO","NGN","NAD","MYR","MXN","MWK","MVR","MUR","MOP","MMK","MKD","MGA","MDL","MAD","LYD","LSL","LKR","LBP","LAK","KZT","KWD","KRW","KMF","KHR","KES","JPY","JOD","JMD","ISK","IQD","INR","ILS","IDR","HUF","HTG","HRK","HNL","HKD","GTQ","GNF","GMD","GBP","EUR","ETB","EGP","DZD","DOP","DKK","DJF","CZK","CVE","CUP","CRC","COP","CNY","CLP","CHF","CAD","BZD","BWP","BSD","BOB","BND","BIF","BHD","BGN","BDT","BBD","AWG","AUD","ARS","ALL","AED","TWD","XOF"],
      ["ZAR","CHF","DKK","INR","SEK","SLL","SOS","SZL","THB","TND","TRY","TTD","TZS","UAH","UGX","UYU","UZS","SGD","VND","XAF","CAD","USD","GBP"])
 
+
 for i in options:
     currencies.append(i + "EUR=X")
 
+st.write("Data Extracted for " + str(len(currencies)) + " Currencies")
 for ccy in currencies:
 	ccy_pair= yf.Ticker(ccy)
 	ccy_pair_list = ccy_pair.history(start=str(start_d) ,end=str(d1),  interval="1d")
